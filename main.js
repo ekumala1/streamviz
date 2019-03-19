c2fbb5f48fad5083cafa55ecc3f23fedf33023a1
Enoch <daneolog@gmail.com>
2019-03-18 22:55:15 -0700

    scatterplot working! (static)

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var data;
 
 var currentTab;
 
+var width;
+var height;
+
 window.onload = function() {
   tabs = document.getElementsByClassName('tab-content');
   tabButtons = document.getElementsByClassName('tab-button');
@@ -11,8 +14,8 @@ window.onload = function() {
     data = results;
     console.log(data);
 
-    viewTab('raw');
-    loadRaw();
+    // just for testing (switch this back to raw if not set)
+    viewTab('scatter');
   });
 };
 
@@ -25,6 +28,12 @@ function viewTab(tabName) {
       tabs[i].style.display = 'none';
       tabButtons[i].className = tabButtons[i].className.replace(' active', '');
     }
+  }
+
+  if (tabName == 'raw') {
+    loadRaw();
+  } else if (tabName == 'scatter') {
+    loadScatter();
   }
 
   currentTab = tabName;
@@ -50,4 +59,44 @@ function loadRaw() {
   }
 
   table.innerHTML = tr;
+}
+
+function loadScatter() {
+  var svg = d3.select('#scattersvg');
+  width = document.getElementById('scattersvg').getBoundingClientRect().width;
+  height = document.getElementById('scattersvg').getBoundingClientRect().height;
+
+  var xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(data, d => +d.Conductivity))
+    .range([50, this.width - 100]);
+
+  var yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(data, d => +d.Turbidity))
+    .range([this.height - 50, 50]);
+
+  var x = d3.axisBottom().scale(xScale);
+  var y = d3.axisLeft().scale(yScale);
+
+  svg
+    .append('g')
+    .attr('transform', `translate(0, ${this.height - 50})`)
+    .call(x);
+  svg
+    .append('g')
+    .attr('transform', 'translate(50, 0)')
+    .call(y);
+
+  var selection = svg.selectAll('.bubble').data(data, d => d);
+
+  selection
+    .enter()
+    .append('circle')
+    .attr('class', 'bubble')
+    .attr('r', 3)
+    .attr('cx', d => xScale(+d.Conductivity))
+    .attr('cy', d => yScale(+d.Turbidity))
+    .style('fill', 'orange')
+    .style('opacity', 0.5);
 }
