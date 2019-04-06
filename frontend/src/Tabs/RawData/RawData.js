2b500817a4a73806cca1ed0101ccae9da1e34a05
Enoch <daneolog@gmail.com>
2019-04-05 20:16:45 -0400

    raw data showing but out of order

--- frontend/src/Tabs/RawData/RawData.js
+++ frontend/src/Tabs/RawData/RawData.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 
 class RawData extends Component {
+  componentDidMount() {
+    fetch('http://localhost:5000/streams')
+      .then(response => response.json())
+      .then(result => {
+        this.setState({ data: result });
+        this.state.data.forEach(d => (d.Date = new Date(d.Date)));
+        console.log(this.state.data);
+      });
+  }
+
+  componentDidUpdate() {
+    let table = '';
+
+    table += '<tr>';
+    for (var key in this.state.data[0]) table += `<th>${key}</th>`;
+    table += '</tr>';
+
+    for (var row of this.state.data) {
+      table += '<tr>';
+      for (var key in row) table += `<td>${row[key]}</td>`;
+      table += '</tr>';
+    }
+
+    this.refs.raw.innerHTML += table;
+  }
+
   render() {
     return (
       <div>
-        <p>placeholder!!!</p>
+        <table>
+          <tbody ref="raw" />
+        </table>
       </div>
     );
   }
