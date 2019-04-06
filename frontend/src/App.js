18bf07c9d1baaaae3228515b4919465f61b9f294
Enoch <daneolog@gmail.com>
2019-04-05 21:44:32 -0400

    time series plot done! back to previous point

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import TopBar from './TopBar/TopBar';
 import BottomBar from './BottomBar/BottomBar';
 import BoxPlot from './Tabs/BoxPlot/BoxPlot';
 import ScatterPlot from './Tabs/ScatterPlot/ScatterPlot';
+import TimeSeries from './Tabs/TimeSeries/TimeSeries';
 import RawData from './Tabs/RawData/RawData';
 
 import './App.css';
@@ -21,7 +22,7 @@ class App extends Component {
             <Route exact path="/" render={() => <Redirect to="/raw" />} />
             <Route path="/box" component={BoxPlot} />
             <Route path="/scatter" component={ScatterPlot} />
-            <Route path="/time" component={RawData} />
+            <Route path="/time" component={TimeSeries} />
             <Route path="/login" component={RawData} />
             <Route path="/raw" component={RawData} />
           </div>
---
+++ frontend/src/Tabs/TimeSeries/TimeSeries.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './TimeSeries.css';
+
+import timePlot from './time';
+import { Dropdown } from 'semantic-ui-react';
+
+class TimeSeries extends Component {
+  params = [];
+
+  constructor() {
+    super();
+    this.state = { keys: [], data: [] };
+  }
+
+  componentDidMount() {
+    this.time = new timePlot();
+    this.time.clear();
+
+    fetch('http://localhost:5000/streams')
+      .then(response => response.json())
+      .then(result => {
+        this.setState({ keys: Object.keys(result[0]), data: result });
+        this.state.data.forEach(d => (d.Date = new Date(d.Date)));
+        console.log(this.state.data);
+
+        this.time.data = this.state.data;
+        this.time.buildAxes();
+      });
+  }
+
+  setParam(event, data) {
+    this.params[0] = data.value;
+    console.log(this.params);
+
+    this.time.buildLinePlot(this.params);
+  }
+
+  render() {
+    var options = this.state.keys.map(key => {
+      return { key: key, text: key, value: key };
+    });
+
+    return (
+      <div className="container">
+        <div className="sidebar">
+          <h2>choices</h2>
+          <p>Variable:</p>
+          <Dropdown
+            placeholder="Variable"
+            fluid
+            selection
+            options={options}
+            onChange={this.setParam.bind(this)}
+          />
+        </div>
+        <div className="content">
+          <svg id="svg" width="1195.5px" height="100%" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TimeSeries;
