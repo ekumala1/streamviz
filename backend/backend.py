a1fa7a2637d7d5ff82123a772f91a7dc83faf045
Enoch <daneolog@gmail.com>
2019-04-05 20:20:34 -0400

    fixed ordering of columns in raw data

--- backend/backend.py
+++ backend/backend.py
@@ -1,9 +1,7 @@
-# export FLASK_APP=backend.py
-# flask run
-
-from flask import Flask, jsonify
+from flask import Flask, redirect, url_for
 from flaskext.mysql import MySQL
 from flask_cors import CORS, cross_origin
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -14,13 +12,24 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql = MySQL(app)
 
 # code from https://stackoverflow.com/questions/29020839/mysql-fetchall-how-to-get-data-inside-a-dict-rather-than-inside-a-tuple/29020853#29020853
+# also deals with organizing data to arrange it in a nice format
 # converts MySQL results into dictionary
 
 
 def get_dict(cursor):
     columns = [col[0] for col in cursor.description]
-    rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
+    rows = []
+    row = cursor.fetchone()
+    while row is not None:
+        row = [None if i is None else str(i) for i in row]
+        rows.append(dict(zip(columns, row)))
+        row = cursor.fetchone()
     return rows
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('streams'))
 
 
 @app.route('/streams')
@@ -28,4 +37,4 @@ def streams():
     conn = mysql.get_db().cursor()
     conn.execute("select * from stream")
 
-    return jsonify(get_dict(conn))
+    return json.dumps(get_dict(conn))
