99ae9aba57ef3923f6be9af10c4be68b610daa50
Enoch Kumala <daneolog@gmail.com>
2019-08-23 15:22:40 -0400

    updated backend url

--- src/Tabs/RawData/RawData.js
+++ src/Tabs/RawData/RawData.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class RawData extends Component {
   componentDidMount() {
-    fetch('http://localhost:8000/streams')
+    fetch("http://localhost:5000/streams")
       .then(response => response.json())
       .then(result => {
         this.setState({ data: result });
@@ -12,16 +12,16 @@ class RawData extends Component {
   }
 
   componentDidUpdate() {
-    let table = '';
+    let table = "";
 
-    table += '<tr>';
+    table += "<tr>";
     for (var key in this.state.data[0]) table += `<th>${key}</th>`;
-    table += '</tr>';
+    table += "</tr>";
 
     for (var row of this.state.data) {
-      table += '<tr>';
+      table += "<tr>";
       for (key in row) table += `<td>${row[key]}</td>`;
-      table += '</tr>';
+      table += "</tr>";
     }
 
     this.refs.raw.innerHTML += table;
